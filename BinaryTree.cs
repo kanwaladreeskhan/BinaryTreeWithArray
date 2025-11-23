45bddb652b8adb5da668ce9be3516a7f91200e78
kanwaladreeskhan <khanadreiskhan@gmail.com>
2025-11-23 09:44:39 +0500

    Initial commit

---
+++ BinaryTree.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeWithArray
+{
+    internal class BinaryTree
+    {
+        static int root = 0;
+        static string[] str = new string[15];
+        public void Setroot(string key)
+        {
+            str[0] = key;
+        }
+        public void Setleft(string key, int root)
+        {
+            int t = (2 * root) + 1;
+            if (str[root] == null)
+            {
+                Console.WriteLine("Could not set child at {0} as no parent is found!");
+            }
+            else
+            {
+                str[t] = key;
+            }
+        }
+        public void Setright(string key, int root)
+        {
+            int t = (2 * root) + 2;
+            if (str[root] == null)
+            {
+                Console.WriteLine("Could not set child at {0} as no parent is found!");
+            }
+            else
+            {
+                str[t] = key;
+            }
+        }
+        public void print()
+        {
+            Console.WriteLine("Array Representation of binary tree is:");
+            for (int i = 0; i < 15; i++)
+            {
+                if (str[i] == null)
+                {
+                    Console.Write("-"+" ");
+                }
+                else
+                {
+                    Console.Write(str[i]+" ");
+                }
+                            }
+            Console.WriteLine();
+        }
+        public void InOrder(int index)
+        {
+         
+            if (index>=15||str[index]==null)
+            {
+                return;
+            }
+            else
+            {
+                InOrder(2 * index + 1);
+                Console.Write(str[index]+" ");
+                InOrder(2 * index + 2);
+            }
+        }
+        public void PreOrder(int index)
+        {
+         
+            if (index >= 15 || str[index] == null)
+            {
+                return;
+            }
+            else
+            {
+                Console.Write(str[index] + " ");
+                PreOrder(2 * index + 1);
+                 PreOrder(2 * index + 2);
+            }
+        }
+        public void PostOrder(int index)
+        {
+            
+            if (index >= 15 || str[index] == null)
+            {
+                return;
+            }
+            else
+            {
+                PostOrder(2 * index + 1);
+                PostOrder(2 * index + 2);
+                Console.Write(str[index] + " ");
+            }
+        }
+        public void Search(string key)
+        {
+            bool found=false;
+           for(int i=0; i<str.Length; i++)
+           {
+                if (str[i] == key)
+                {
+                    Console.WriteLine("{0} is at index {1}.", str[i], i);
+                    found = true;
+                }
+                   
+           }
+            if (!found)
+            {
+                Console.WriteLine("Element not found in tree!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeWithArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree=new BinaryTree();
+            tree.Setroot("A");
+            tree.Setleft("B",0);
+            tree.Setright("C",0);
+            tree.Setleft("D",1);
+            tree.Setright("E",2);
+            tree.Setleft("F",6);
+            tree.Setright("G", 6);
+            tree.print();
+            Console.WriteLine("InOrder Traversal:");
+            tree.InOrder(0);
+            Console.WriteLine();
+            Console.WriteLine("PreOrder Traversal:");
+            tree.PreOrder(0);
+            Console.WriteLine();
+            Console.WriteLine("PostOrder Traversal:");
+            tree.PostOrder(0);
+            Console.WriteLine();
+            tree.Search("D");
+        }
+    }
+}
